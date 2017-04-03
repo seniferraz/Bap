6914408f0a9d2f96b9f26159a319a08b92d0e53f
diogosferreira <diogoefre>
2017-04-03 19:07:12 +0100

    O

--- javaScript/search.js
+++ javaScript/search.js
@@ -1,7 +1,7 @@
 // Nome do utilizador a analisar
 var targetUserName = $("#name").val(); //texto (val) que foi escrito na caixa de pesquisa
-var targetUserName11 = $("#location").val(); 
-var targetUserName12 = $("#fields").val(); 
+var Location = $("#location").val(); 
+var fields = $("#fields").val(); 
  
 // Array associativo com nome do utilizador como chave e valor de amizade como valor
 var amigos = {};
